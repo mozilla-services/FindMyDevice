b533fc650c25b8314d6b1f6d2a3536c1bc5c7c60
jr conlin <jrconlin@gmail.com>
2013-11-26 21:57:13 -0800

    In progress - more meat, Night check

--- src/mozilla.org/wmf/handlers.go
+++ src/mozilla.org/wmf/handlers.go
@@ -24,10 +24,6 @@ func NewHandler (config util.JsMap, logger *util.HekaLogger, store *storage.Stor
         store: store}
 }
 
-func (self *Handler) decodeData(body []byte) (reply map[string]interface{}, err error) {
-    return
-}
-
 func (self *Handler) Register(resp http.ResponseWriter, req *http.Request) {
     /*register a new device
     */
@@ -40,18 +36,20 @@ func (self *Handler) Register(resp http.ResponseWriter, req *http.Request) {
     var lockable bool
     var ok bool
 
+    logCat := "handler:Register"
+
     if body, err := ioutil.ReadAll(req.Body); err != nil {
         http.Error(resp, "No body", http.StatusBadRequest)
     } else {
         err := json.Unmarshal(body, &buffer)
         if err != nil {
-            self.logger.Error("handler", "Unparsable data",
+            self.logger.Error(logCat, "Unparsable data",
                 util.Fields{"raw": string(body), "error":err.Error()})
             http.Error(resp, "Bad Data", 400)
             return
         }
         if _, ok := buffer["assertion"]; !ok {
-            self.logger.Error("handler", "Missing assertion", nil)
+            self.logger.Error(logCat, "Missing assertion", nil)
             http.Error(resp, "Unauthorized", 401)
             return
         } else {
@@ -60,14 +58,14 @@ func (self *Handler) Register(resp http.ResponseWriter, req *http.Request) {
         }
 
         if _, ok = buffer["pushurl"]; !ok {
-            self.logger.Error("handler", "Missing SimplePush url", nil)
+            self.logger.Error(logCat, "Missing SimplePush url", nil)
             http.Error(resp, "Bad Data", 400)
             return
         } else {
             pushUrl = buffer["pushurl"].(string)
         }
         if _, ok = buffer["secret"]; !ok {
-            self.logger.Error("handler", "Missing HAWK secret", nil)
+            self.logger.Error(logCat, "Missing HAWK secret", nil)
             http.Error(resp, "Bad Data", 400)
             return
         } else {
@@ -93,12 +91,12 @@ func (self *Handler) Register(resp http.ResponseWriter, req *http.Request) {
             PushUrl: pushUrl,
             Lockable: lockable,
         }); err != nil {
-            self.logger.Error("handler", "Error storing data", nil)
+            self.logger.Error(logCat, "Error storing data", nil)
             http.Error(resp, "Server error", 500)
             return
         } else {
         if devId != deviceid {
-            self.logger.Error("handler", "Different deviceID returned",
+            self.logger.Error(logCat, "Different deviceID returned",
                 util.Fields{"original": deviceid, "new": devId})
             http.Error(resp, "Server error", 500)
             return
@@ -109,26 +107,33 @@ func (self *Handler) Register(resp http.ResponseWriter, req *http.Request) {
 
 }
 
-func (self *Handler) Login(resp http.ResponseWriter, req *http.Request) {
-    /* Handle a user login to the web UI
-    */
-
-}
-
 func (self *Handler) Cmd(resp http.ResponseWriter, req *http.Request) {
     /* Pass the latest command off to the device.
     */
-    // get the record for the deviceID
-    deviceId := strings.Split(req.URL.Path,"/")[1]
-    //decode the body
     var body [1024]byte
     var err error
     var l int
-    
+
+    logCat := "handler:Cmd"
+    // get the record for the deviceID
+    deviceId := strings.Split(req.URL.Path,"/")[1]
+    if deviceId == "" {
+        self.logger.Error(logCat, "Invalid call (No device id)", nil)
+        http.Error(resp, "Unauthorized", 401)
+        return
+    }
+    if self.store.ValidateDevice(deviceId) == false {
+        self.logger.Error(logCat, "Cmd:Unknown device requesting cmd", util.Fields{
+                "deviceId": deviceId})
+        http.Error(resp, "Unauthorized", 401)
+        return
+    }
+    //decode the body
     l, err = req.Body.Read(body)
     if l > 0 {
         reply := make(map[string]interface{})
         merr := json.Unmarshal(body, reply)
+        // handle command acks
         for cmd, args := range reply {
             switch cmd {
             case "l":
@@ -141,19 +146,50 @@ func (self *Handler) Cmd(resp http.ResponseWriter, req *http.Request) {
                 // erase
             case "t":
                 // track
+                // store tracking info.
         }
     }
-    
-    resp.Write([]byte("OK"))
-    
+
+    // reply with pending commands
+    // 
+    cmd, err := json.Marshall(self.store.GetPending(deviceId))
+    if err == nil {
+        resp.Write(cmd)
+    } else {
+        self.logger.Error(logCat, "Could not send commands", util.Fields{"error":err}) 
+    }
+    resp.Write([]byte("{}"))
+
+}
+
+// user login functions
+
+func (self *Handler) Login(resp http.ResponseWriter, req *http.Request) {
+    /* Handle a user login to the web UI
+    */
+    // validate the assertion
+    // get/create the userid
+    // create the user record (if not already present)
+    // send the device update request.
+    // show the map/command page.
 
 }
 
 func (self *Handler) State(resp http.ResponseWriter, req *http.Request) {
     /* Show the state of the user's devices.
     */
+    // get session info
 }
 
+func (self *Handler) SendCmd(resp http.ResponseWriter, req *http.Request) {
+    /* queue a command to a device
+    */
+    // is the user logged in?
+        // no, they fail
+    // validate the command and args
+    // add to device's queue
+
+}
 func (self *Handler) StatusHandler(resp http.ResponseWriter, req *http.Request) {
     /* Show program status
     */
