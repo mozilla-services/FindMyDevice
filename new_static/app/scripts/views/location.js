1616ae66ae77c3a35c9cfaaf29a7f622de00f48a
Nick Chapman <nchapman@mykolab.com>
2014-03-06 14:00:16 -0800

    Hooked up basic events.

--- new_static/app/scripts/views/location.js
+++ new_static/app/scripts/views/location.js
@@ -9,6 +9,24 @@ define([
   var LocationView = BaseView.extend({
     template: LocationTemplate,
 
+    events: {
+      'click span.play-sound': 'playSound',
+      'click span.lost-mode': 'lostMode',
+      'click span.erase': 'erase'
+    },
+
+    playSound: function(event) {
+      alert("PLAYING THE SOUND");
+    },
+
+    lostMode: function(event) {
+      alert("LOSING THE DEVICE");
+    },
+
+    erase: function(event) {
+      alert("ERASING THE DEVICE");
+    },
+
     afterInsert: function() {
       L.mapbox.map('map', 'nchapman.hejm93ej');
     }
