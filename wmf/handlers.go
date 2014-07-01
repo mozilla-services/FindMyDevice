c6751e5decabb6ff2251fa3b923e3b0a411aae99
jrconlin <jconlin+git@mozilla.com>
2014-07-01 08:12:56 -0700

    Fix to debug statement

--- wmf/handlers.go
+++ wmf/handlers.go
@@ -670,7 +670,7 @@ func (self *Handler) updatePage(devId, cmd string, args map[string]interface{}, 
 	muClient.Lock()
 	if clients, ok := Clients[devId]; ok {
 		js, _ := json.Marshal(location)
-		fmt.Printf("### Sending update to %d pages: %s : %+s\n", len(clients), js)
+		fmt.Printf("### Sending update to %d pages: %s\n", len(clients), js)
 		for _, i := range clients {
 			i.Write(js)
 		}
