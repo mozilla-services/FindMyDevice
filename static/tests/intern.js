1565a674a3d5ed1182b125c10bd678688fec3660
Nick Chapman <nchapman@mykolab.com>
2014-07-16 16:17:53 -0700

    Cleaned up intern.js.

--- static/tests/intern.js
+++ static/tests/intern.js
@@ -39,41 +39,16 @@ define(['intern/lib/args'], function (args) {
     // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
     // capabilities options specified for an environment will be copied as-is
     environments: [
-      // { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-      // { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-      // { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-      // { browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-      // { browserName: 'chrome', version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-      // { browserName: 'safari', version: '6', platform: 'OS X 10.8' },
-      // { browserName: 'safari', version: '7', platform: 'OS X 10.9' },
       { browserName: 'firefox' }
     ],
 
     // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
-    maxConcurrency: 1,
+    maxConcurrency: 3,
 
-    reporters: ['console'],
+    reporters: [ 'console' ],
 
-    // Name of the tunnel class to use for WebDriver tests
-    //tunnel: 'SauceLabsTunnel',
-    tunnel: 'NullTunnel',
-
-    // The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
-    // loader
-    useLoader: {
-      'host-node': 'dojo/dojo',
-      'host-browser': 'node_modules/dojo/dojo.js'
-    },
-
-    // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
-    // can be used here
-    loader: {
-      // Packages that should be registered with the loader in each testing environment
-      packages: [ { name: 'app', location: 'app' } ]
-    },
-
-    // Non-functional test suite(s) to run in each browser
-    suites: [ /* 'myPackage/tests/foo', 'myPackage/tests/bar' */ ],
+    // Whether or not to start Sauce Connect before running tests
+    useSauceConnect: false,
 
     // Functional test suite(s) to run in each browser once non-functional tests are completed
     functionalSuites: [
